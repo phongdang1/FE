eda1b1ca97c5ed09f15a09bd8c0e425d31082e05
phongdang1 <phongdbde160574@fpt.edu.vn>
2024-11-23 09:25:11 +0700

    test5

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/auth/google": {
-        target: "http://localhost:5000",
+        target: "https://be-6414.onrender.com/",
         changeOrigin: true,
         secure: false,
       },
