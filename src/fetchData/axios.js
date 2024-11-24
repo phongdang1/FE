f1a791ed2ed14ed5344fd03c19901306989e4fce
phongdang1 <phongdbde160574@fpt.edu.vn>
2024-11-24 21:16:58 +0700

    test11

--- src/fetchData/axios.js
+++ src/fetchData/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://be-6414.onrender.com/',
+    baseURL: 'https://be-6414.onrender.com',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
